11433ddad90322fe3d20afe84438ffbcf192ca48
Bruce <recall4056@gmail.com>
2023-09-05 01:01:12 +0800

    chore: add vue dev tool

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import Icons from 'unplugin-icons/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -79,7 +80,8 @@ export default defineConfig(({ mode }) => {
         ],
         dirs: ['src/components']
       }),
-      Icons({ autoInstall: true })
+      Icons({ autoInstall: true }),
+      VueDevTools()
     ],
     resolve: {
       alias: {
